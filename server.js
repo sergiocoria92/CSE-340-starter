d3ef23a46929efbb99e8c31c4b3ef1f1faffc826
Sergio Coria <sergiocoria.my@gmail.com>
2025-09-11 01:12:03 -0600

    feat(home): nav activo, imágenes correctas y estilos

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ const expressLayouts = require('express-ejs-layouts')
 
 const app = express()
 
-// Motor de vistas
+// Vistas + layouts
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(expressLayouts)
-app.set('layout', './layouts/layout') // relativo a /views
+app.set('layout', './layouts/layout')
 
-// Archivos estáticos (CSS/imagenes)
+// Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')))
 
-// Home
+// Home  ⬅️ aquí pasamos active: 'home'
 app.get('/', (req, res) => {
-  res.render('index', { title: 'CSE Motors' })
+  res.render('index', { title: 'CSE Motors', active: 'home' })
 })
 
 // Salud (opcional)
